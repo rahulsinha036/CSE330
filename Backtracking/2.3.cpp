8c2265ae8f3ea26767233cfed5e0ab4183c6a841
Rahul Sinha <54495328+rahulsinha036@users.noreply.github.com>
2022-09-15 13:51:35 +0530

    update

--- Backtracking/2.3.cpp
+++ Backtracking/2.3.cpp
@@ -1,3 +1,13 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
 class Solution
 {
 public:
@@ -23,3 +33,37 @@ void solve(vector<vector<int>>&grid,int row,int col,int n,int m,vector<int>temp,
        return store;
     }
 };
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> grid(n, vector<int>(m));
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                cin >> grid[i][j];
+            }
+        }
+        Solution ob;
+        auto ans = ob.findAllPossiblePaths(n, m, grid);
+        for (int i = 0; i < ans.size(); i++)
+        {
+            for (int j = 0; j < ans[i].size(); j++)
+            {
+                cout << ans[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+    return 0;
+}
+
+// } Driver Code Ends
