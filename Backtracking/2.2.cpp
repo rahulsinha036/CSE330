965bbfa8027f27354313bbd1b49ef55ecba76843
Rahul Sinha <54495328+rahulsinha036@users.noreply.github.com>
2022-09-15 13:51:03 +0530

    update

--- Backtracking/2.2.cpp
+++ Backtracking/2.2.cpp
@@ -1,25 +1,55 @@
-class Solution
-{
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
 public:
-void solve(vector<vector<int>>&grid,int row,int col,int n,int m,vector<int>temp,vector<vector<int>>&ans){
-    if(row>=n||col>=m){
-        return;
+vector<string> v;
+    void solve(string s, string output){
+        
+        if(s.empty()){
+            v.push_back(output);
+            return;
+        }
+
+        output.push_back(' ');
+        output.push_back(s[0]);
+        solve(s.substr(1),output);
+        
+        output.pop_back();
+        output.pop_back();
+        output.push_back(s[0]);
+        solve(s.substr(1),output);
+
     }
-    temp.push_back(grid[row][col]);
-    if(row==n-1 && col==m-1){
-        ans.push_back(temp);
-        return;
-    }
-    solve(grid,row,col+1,n,m,temp,ans);
-    solve(grid,row+1,col,n,m,temp,ans);
-    temp.pop_back();
-}
-    vector<vector<int>> findAllPossiblePaths(int n, int m, vector<vector<int>> &grid)
-    {
-       vector<vector<int>>store;
-       vector<int>temp;
-       solve(grid,0,0,n,m,temp,store);
-       reverse(store.begin(),store.end());
-       return store;
+    vector<string> permutation(string S){
+        string output="";
+        output.push_back(S[0]);
+        
+        solve(S.substr(1),output);
+        
+        return v;
     }
 };
+
+//{ Driver Code Starts.
+
+int  main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string S;
+        cin>>S;
+        vector<string> ans;
+        Solution obj;
+        ans = obj.permutation(S);
+        for(int i=0;i<ans.size();i++){
+            cout<<"("<<ans[i]<<")";
+        }
+        cout << endl;
+    }
+}
+
+// } Driver Code Ends
