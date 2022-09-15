252f318fbd136a734c61477646f88e6c79713405
Rahul Sinha <54495328+rahulsinha036@users.noreply.github.com>
2022-09-15 13:50:04 +0530

    update

--- Backtracking/2.1.cpp
+++ Backtracking/2.1.cpp
@@ -1,25 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
 class Solution
 {
-public:
-void solve(vector<vector<int>>&grid,int row,int col,int n,int m,vector<int>temp,vector<vector<int>>&ans){
-    if(row>=n||col>=m){
-        return;
-    }
-    temp.push_back(grid[row][col]);
-    if(row==n-1 && col==m-1){
-        ans.push_back(temp);
-        return;
-    }
-    solve(grid,row,col+1,n,m,temp,ans);
-    solve(grid,row+1,col,n,m,temp,ans);
-    temp.pop_back();
-}
-    vector<vector<int>> findAllPossiblePaths(int n, int m, vector<vector<int>> &grid)
-    {
-       vector<vector<int>>store;
-       vector<int>temp;
-       solve(grid,0,0,n,m,temp,store);
-       reverse(store.begin(),store.end());
-       return store;
-    }
+	public:
+		vector<string>find_permutation(string S)
+		{
+		    sort(S.begin(),S.end());
+            vector<string> v;
+            int i;
+            do{
+                v.push_back(S);
+             }
+             while(next_permutation(S.begin(),S.end()));
+             return v;
+		}
 };
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--)
+    {
+	    string S;
+	    cin >> S;
+	    Solution ob;
+	    vector<string> ans = ob.find_permutation(S);
+	    for(auto i: ans)
+	    {
+	    	cout<<i<<" ";
+	    }
+	    cout<<"\n";
+    }
+	return 0;
+}
+
+// } Driver Code Ends
