b115f84975a4b87595b8aacd41c8063f9a252085
Rahul Sinha <54495328+rahulsinha036@users.noreply.github.com>
2022-09-15 13:14:31 +0530

    initial commit

---
+++ 1.9.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n;
+int t;
+cin>>t;
+while(t--)
+{
+cin>>n;
+if(n==2)
+{
+	cout<<"Arjit"<<endl;
+}
+else
+{
+	cout<<"Deepa"<<endl;
+}
+}
+}
