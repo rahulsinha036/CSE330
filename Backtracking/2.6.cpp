76809bcbc1e3b67f728e579e6752ca9a3841e5b4
Rahul Sinha <54495328+rahulsinha036@users.noreply.github.com>
2022-09-29 08:21:43 +0530

    Update 2.6.cpp

--- Backtracking/2.6.cpp
+++ Backtracking/2.6.cpp
@@ -1,55 +1,90 @@
 //{ Driver Code Starts
+// Initial Template for C++
+
 #include <bits/stdc++.h>
 using namespace std;
 
 // } Driver Code Ends
-
+// User function Template for C++
 
 class Solution{
-    public:
-      long long int numberOfways(int a[], int n)
-   {
-       // Complete the function
-       int sum=n;
-       int count=0;
-       int sum1=0;
-       for(int i=0;i<n;i++)
-       {
-           if(a[i]==a[i+1])
-           {
-               count++;
-           }
-           else
-           {
-               sum1=(count *(count+1))/2;
-               sum+=sum1;
-               count=0;
-           }
-           
-       }
-       sum1=(count *(count+1))/2;
-       sum+=sum1;
-       return sum;
-       
-   }
+public:
+    void solve(int col, vector<bool> &leftRow, vector<bool> &upperDiagonal, vector<bool> &lowerDiagonal, vector<int> &arr, vector<vector<int>> &res) {
+
+        if(col == leftRow.size()) {
+
+            res.push_back(arr);
+
+            return;
+
+        }
+
+        
+
+        for(int row = 0; row < leftRow.size(); row++) {
+
+            if(!leftRow[row] && !upperDiagonal[row+col] && !lowerDiagonal[leftRow.size()-1+col-row]) {
+
+                leftRow[row] = upperDiagonal[row+col] = lowerDiagonal[leftRow.size()-1+col-row] = true;
+
+                arr.push_back(row+1);
+
+                solve(col+1, leftRow, upperDiagonal, lowerDiagonal, arr, res);
+
+                arr.pop_back();
+
+                leftRow[row] = upperDiagonal[row+col] = lowerDiagonal[leftRow.size()-1+col-row] = false;
+
+            }
+
+        }
+
+    }
+
+    vector<vector<int>> nQueen(int n) {
+
+        vector<vector<int>> res;
+
+        vector<int> arr;
+
+        vector<bool> leftRow(n);
+
+        vector<bool> upperDiagonal(2*n-1);
+
+        vector<bool> lowerDiagonal(2*n-1);
+
+        
+
+        solve(0, leftRow, upperDiagonal, lowerDiagonal, arr, res);
+
+        return res;
+
+    }
 };
 
 //{ Driver Code Starts.
-int main()
-{
-	int t;
-	cin>>t;
-	while(t--)
-	{
-	    int n;
-	    cin>>n; 
-	    int a[n];
-	    for(int i=0;i<n;i++)
-	        cin>>a[i];
-	    Solution ob;
-	    cout << ob.numberOfways(a, n)<<"\n";
-     }
-	return 0;
-}
 
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        
+        Solution ob;
+        vector<vector<int>> ans = ob.nQueen(n);
+        if(ans.size() == 0)
+            cout<<-1<<"\n";
+        else {
+            for(int i = 0;i < ans.size();i++){
+                cout<<"[";
+                for(int u: ans[i])
+                    cout<<u<<" ";
+                cout<<"] ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
 // } Driver Code Ends
